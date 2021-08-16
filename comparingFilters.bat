f8aea8004bd9376e0f20ed6da0ac949bdf8bde72
Alfonso Sánchez-Macián <alfonso@sanchezmacian.com>
2021-08-16 16:20:31 +0200

    Modifying paths

--- comparingFilters.bat
+++ comparingFilters.bat
@@ -1,8 +1,7 @@
 ECHO Start of Loop
 
 set GUAVA_DIR=./data/guava-30.1.1-jre.jar
-#set FASTFILTER_DIR=../fastfilter_java/fastfilter/bin
-set FASTFILTER_DIR=C:\Users\Alfonso\Documents\Research\ProbabilisticStructures\fastfilter_java\fastfilter\bin
+set FASTFILTER_DIR=../fastfilter_java/fastfilter/bin
 
 set CLASSPATH=./bin;%GUAVA_DIR%;%FASTFILTER_DIR%
 
